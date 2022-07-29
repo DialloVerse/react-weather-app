0ca1068efe23b28e3b2037a57bd504e3f80430f4
DialloVerse <durali@ymail.com>
2022-07-29 21:36:28 +0200

    Bug notes

--- src/index.js
+++ src/index.js
@@ -13,5 +13,6 @@ root.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
+// If you input {weather.sys.country.result}, it'll eventually display the countries without the abbrv in front...newly noted for future ref.
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
